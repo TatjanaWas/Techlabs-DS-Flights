c7d9e07b3b95a026adc6820c8414b99163d06e28
Backtash <back20012@gmail.com>
2024-03-07 17:47:40 +0100

    commit 9 millionkjasdkja

--- code/correct.py
+++ code/correct.py
@@ -4,6 +4,6 @@ with open('../code/flights_prediction.ipynb', 'r') as f:
     lines = f.readlines()
 
 # replace with new notebook
-with open('../code/flights_prediction.ipynb', 'w') as f:
+with open('flights_prediction.ipynb', 'w') as f:
     for line in lines:
         f.write(line.replace('"execution_count": "null"', '"execution_count": null'))
